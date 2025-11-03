a6c1c5c5143e9d72d600589466d3b95236ff04b4
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-03 12:00:25 -0500

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BackgroundSquares from './Components/BackgroundSquares/BackgroundSquares'
 
 function App() {
 
   return (
-    <div>Test</div>
+    <div>
+      <BackgroundSquares />
+    </div>
   )
 }
 
--- src/Components/BackgroundSquares/BackgroundSquares.jsx
+++ src/Components/BackgroundSquares/BackgroundSquares.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { use } from 'react'
 import '../BackgroundSquares/BackgroundSquares.css'
+import { useState } from 'react'
+import { useEffect } from 'react';
 
 function BackgroundSquares() {
+    const [totalSquares, setTotalSquares] = useState(0);
+
+    const getTotalSquares = () => {
+        const windowHeight = window.innerHeight;
+        const windowWidth = window.innerWidth;
+        console.log("Window height:", windowHeight);
+        console.log("Window width:", windowWidth);
+
+        const squaresPerRow = Math.floor(windowWidth / 100); // Since each square is 100px by 100px
+        const squaresPerColumn = Math.floor(windowHeight / 100);
+        setTotalSquares(squaresPerColumn * squaresPerRow);
+        console.log("Squares per row:", squaresPerRow);
+        console.log("Squares per column:", squaresPerColumn);
+
+    }
+
+    useEffect(() => {
+        getTotalSquares();
+    }, [])
+
   return (
-    <div>BackgroundSquares</div>
+    <div id='background-squares-container'>
+        <div className='background-square'></div>
+        
+    </div>
   )
 }
 
