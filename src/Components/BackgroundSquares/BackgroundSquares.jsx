785f5ae02f1757ed3ed3b26c57cc5d1fefc18422
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-02 22:35:33 -0500

    Updated files

---
+++ src/Components/BackgroundSquares/BackgroundSquares.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../BackgroundSquares/BackgroundSquares.css'
+
+function BackgroundSquares() {
+  return (
+    <div>BackgroundSquares</div>
+  )
+}
+
+export default BackgroundSquares
