402af84148a72eb8ecbaa88187c2c8f53d74a9fb
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-03 14:39:48 -0500

    updated grid

--- src/Components/BackgroundSquares/BackgroundSquares.jsx
+++ src/Components/BackgroundSquares/BackgroundSquares.jsx
@@ -9,25 +9,33 @@ function BackgroundSquares() {
     const getTotalSquares = () => {
         const windowHeight = window.innerHeight;
         const windowWidth = window.innerWidth;
-        console.log("Window height:", windowHeight);
-        console.log("Window width:", windowWidth);
+        //console.log("Window height:", windowHeight);
+        //console.log("Window width:", windowWidth);
 
         const squaresPerRow = Math.floor(windowWidth / 100); // Since each square is 100px by 100px
         const squaresPerColumn = Math.floor(windowHeight / 100);
         setTotalSquares(squaresPerColumn * squaresPerRow);
-        console.log("Squares per row:", squaresPerRow);
-        console.log("Squares per column:", squaresPerColumn);
+        console.log(totalSquares);
+        //console.log("Squares per row:", squaresPerRow);
+        //console.log("Squares per column:", squaresPerColumn);
+    }
 
+    const handleWindowResize = () => {
+        getTotalSquares();
     }
 
     useEffect(() => {
         getTotalSquares();
-    }, [])
+        window.addEventListener("resize", getTotalSquares);
+        return () => window.removeEventListener("resize", getTotalSquares);
+    }, [totalSquares])
 
   return (
     <div id='background-squares-container'>
         <div className='background-square'></div>
-        
+        {Array.from({ length: totalSquares }).map((item, index) => {
+            return (<div key={index} className='background-square'></div>)
+        })}
     </div>
   )
 }
