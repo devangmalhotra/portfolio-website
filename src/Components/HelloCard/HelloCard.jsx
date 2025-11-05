b7b2976c748a14d72084a40faa087e461b61c693
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-05 11:57:35 -0500

    updated files

--- src/Components/HelloCard/HelloCard.jsx
+++ src/Components/HelloCard/HelloCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import '../HelloCard/HelloCard.css'
+import ContactBtn from '../ContactBtn/ContactBtn'
 
 function HelloCard() {
   return (
-    <div>HelloCard</div>
+    <div id='hello-card-container'>
+      <div id='hello-card-content'>
+        <h2>Hi There, I'm Devang Malhotra.</h2>
+        <h5>4th Year BBA and CS Student.</h5>
+        <ContactBtn />
+      </div>
+    </div>
   )
 }
 
