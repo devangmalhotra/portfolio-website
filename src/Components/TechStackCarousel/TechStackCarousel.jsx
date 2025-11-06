bb3e0b7c9d2cce9bba1d8f1236c7baefb510b6ea
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-06 15:05:05 -0500

    updated files

--- src/Components/TechStackCarousel/TechStackCarousel.jsx
+++ src/Components/TechStackCarousel/TechStackCarousel.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import './TechStackCarousel.css'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import { useRef } from 'react';
+import pythonLogo from '../../assets/TechStackIcons/icons8-python.svg'
+import htmlLogo from '../../assets/TechStackIcons/icons8-html-5.svg'
+import cssLogo from '../../assets/TechStackIcons/icons8-css3.svg'
+import jsLogo from '../../assets/TechStackIcons/icons8-javascript.svg'
+import javaLogo from '../../assets/TechStackIcons/icons8-java.svg'
+import sqlLogo from '../../assets/TechStackIcons/icons8-sql-100.png'
 
 function TechStackCarousel() {
-    const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 0,
-    cssEase: 'linear',
-    pauseOnHover: true,
-    arrows: false,
-  };
-
-  let sliderRef = useRef(null);
-
   return (
     <div className='logos'>
         <div className='logos-slide'>
-            <img src="" alt="" />
+            <img src={pythonLogo} alt="Python" />
+            <img src={htmlLogo} alt="HTML5" />
+            <img src={cssLogo} alt="CSS3" />
+            <img src={jsLogo} alt="JavaScript" />
+            <img src={javaLogo} alt="Java" />
+            <img src={sqlLogo} alt="SQL" />
+        </div>
+        <div className='logos-slide'>
+            <img src={pythonLogo} alt="Python" />
+            <img src={htmlLogo} alt="HTML5" />
+            <img src={cssLogo} alt="CSS3" />
+            <img src={jsLogo} alt="JavaScript" />
+            <img src={javaLogo} alt="Java" />
+            <img src={sqlLogo} alt="SQL" />
         </div>
     </div>
   )
