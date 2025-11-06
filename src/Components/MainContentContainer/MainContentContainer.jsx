d8f8b378b0dc6e7430441af499492e866ee374c5
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-05 22:22:33 -0500

    updated files

--- src/Components/MainContentContainer/MainContentContainer.jsx
+++ src/Components/MainContentContainer/MainContentContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './MainContentContainer.css'
 import HelloCard from '../HelloCard/HelloCard'
+import TechStackCard from '../TechStackCard/TechStackCard'
 
 function MainContentContainer() {
   return (
     <div id='main-content-container'>
         <HelloCard />
+        <TechStackCard />
     </div>
   )
 }
---
+++ src/Components/TechStackCard/TechStackCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TechStackCard.css'
+
+function TechStackCard() {
+  return (
+    <div id='tech-stack-card-container'></div>
+  )
+}
+
+export default TechStackCard
