8707348c13fb1f23b9f280fd3d959ec25fc649e1
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-07 21:18:06 -0500

    updated and added files

--- src/Components/MainContentContainer/MainContentContainer.jsx
+++ src/Components/MainContentContainer/MainContentContainer.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './MainContentContainer.css'
 import HelloCard from '../HelloCard/HelloCard'
 import TechStackCard from '../TechStackCard/TechStackCard'
+import NavBar from '../NavBar/NavBar'
 
 function MainContentContainer() {
   return (
     <div id='main-content-container'>
         <HelloCard />
         <TechStackCard />
+        <NavBar />
     </div>
   )
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div id='navbar-card-container'>NavBar</div>
+  )
+}
+
+export default NavBar
