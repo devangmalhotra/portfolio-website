ba623288f6a70c3324b7b5b685f12e37ef1a031a
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-09 14:42:33 -0500

    updated files

--- src/Components/GithubCard/GithubCard.jsx
+++ src/Components/GithubCard/GithubCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './GithubCard.css'
+import githubLogo from '../../assets/icons8-github-64.png'
 
 function GithubCard() {
+    const handleClick = () => {
+        const newWindow = window.open('https://github.com/devangmalhotra', '_blank', 'noopener,noreferrer');
+        if (newWindow) newWindow.opener = null;
+    };
+
   return (
-    <div id='github-card-container' className='external-link-card'>GithubCard</div>
+    <div id='github-card-container' className='external-link-card' onClick={handleClick}>
+        <div id='github-card-content' className='external-link-card-content'>
+            <img src={githubLogo} alt="" />
+        </div>
+    </div>
   )
 }
 
