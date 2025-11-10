2873c7900d846990e8d181bf87756194d0e79d8c
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-10 10:52:40 -0500

    updated files

--- src/Components/GithubCard/GithubCard.jsx
+++ src/Components/GithubCard/GithubCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './GithubCard.css'
 import githubLogo from '../../assets/icons8-github-64.png'
+import externalLinkIcon from '../../assets/icons8-external-link-128.png'
 
 function GithubCard() {
     const handleClick = () => {
@@ -11,6 +12,9 @@ function GithubCard() {
   return (
     <div id='github-card-container' className='external-link-card' onClick={handleClick}>
         <div id='github-card-content' className='external-link-card-content'>
+            <div className='external-link-icon-container'>
+                <img src={externalLinkIcon} alt="" />
+            </div>
             <img src={githubLogo} alt="" />
         </div>
     </div>
