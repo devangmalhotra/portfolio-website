dd699707507e4bd5815cb5d7f0b101e91819a79d
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-12 16:28:53 -0500

    updated files

--- src/Components/Contact/EmailCard/EmailCard.jsx
+++ src/Components/Contact/EmailCard/EmailCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './EmailCard.css'
+import emailIcon from '../../../assets/ContactIcons/icons8-email-48.png'
 
 function EmailCard() {
   return (
     <div id='email-card-container'>
-        <div id='email-card-content'></div>
+        <div id='email-card-content'>
+            <img src={emailIcon} alt="" /><h4>malh2226@mylaurier.ca</h4>
+        </div>
     </div>
   )
 }
---
+++ src/Components/Contact/PhoneCard/PhoneCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PhoneCard.css'
+
+function PhoneCard() {
+  return (
+    <div>PhoneCard</div>
+  )
+}
+
+export default PhoneCard
