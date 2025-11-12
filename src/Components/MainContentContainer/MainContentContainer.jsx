36ea52c1a7ba881088b2e35299e086cb91952fe5
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-12 14:31:27 -0500

    updated files

--- src/Components/MainContentContainer/MainContentContainer.jsx
+++ src/Components/MainContentContainer/MainContentContainer.jsx
@@ -17,17 +17,33 @@ function MainContentContainer() {
         console.log("Section changed to:", section);
     }
 
-  return (
-    <div id='main-content-container'>
-        <NavBar handleSectionClick={handleSectionClick} activeSection={activeSection}/>
-        <div id='pages-container'>
-            <div id='home-page' className={activeSection == 'home' ? 'page' : 'page inactive-page'}>
+    const sections = [{ name: 'home', content: (
+    <div id='home-page' className={activeSection == 'home' ? 'page' : 'page inactive-page'}>
                 <HelloCard />
                 <TechStackCard />
                 <LocationCard />
                 <GithubCard />
                 <LinkedinCard />
             </div>
+            ) }, { name: 'experience', content: (
+    <div id='experience-page' className={activeSection == 'experience' ? 'page' : 'page inactive-page'}>
+                <HelloCard />
+                <TechStackCard />
+                <LocationCard />
+                <GithubCard />
+                <LinkedinCard />
+            </div>
+            ) }]
+
+    const renderActiveSection = () => {
+        return sections.find(section => section.name === activeSection).content
+    }
+
+  return (
+    <div id='main-content-container'>
+        <NavBar handleSectionClick={handleSectionClick} activeSection={activeSection}/>
+        <div id='pages-container'>
+            {renderActiveSection()}
         </div>
     </div>
   )
