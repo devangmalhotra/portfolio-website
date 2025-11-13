aa83d8718239b9d287e1376387cdc1da74831ec3
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-13 15:48:39 -0500

    updated files

--- src/Components/Contact/EmailCard/EmailCard.jsx
+++ src/Components/Contact/EmailCard/EmailCard.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import './EmailCard.css'
 import emailIcon from '../../../assets/ContactIcons/icons8-email-48.png'
-import externalLinkIcon from '../../../assets/icons8-external-link-128.png'
+import CopyClipboardBtn from '../../CopyClipboardBtn/CopyClipboardBtn'
 
 function EmailCard() {
-  const handleClick = () => {
-        const newWindow = window.open('mailto:malh2226@mylaurier.ca', '_blank', 'noopener,noreferrer');
-        if (newWindow) newWindow.opener = null;
-    };
-
   return (
-    <div id='email-card-container' className='card external-link' onClick={handleClick}>
-        <div className='external-link-icon-container'>
-            <img src={externalLinkIcon} alt="" />
-        </div>
+    <div id='email-card-container' className='card'>
         <div id='email-card-content' className='card-content'>
             <img src={emailIcon} alt="" /><h4>malh2226@mylaurier.ca</h4>
+            <CopyClipboardBtn text='malh2226@mylaurier.ca' />
         </div>
     </div>
   )
---
+++ src/Components/CopyClipboardBtn/CopyClipboardBtn.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import './CopyClipboardBtn.css'
+import clipboardIcon from '../../assets/icons8-copy-16.png'
+
+function CopyClipboardBtn(props) {
+    const [animating, setAnimating] = useState(false);
+
+    const handleClick = async () => {
+        await navigator.clipboard.writeText(props.text);
+        setAnimating(true);
+    }
+
+  return (
+    <div id='clipboard-btn-container'>
+        <button id='clipboard-btn' onClick={handleClick}>
+            <img src={clipboardIcon} alt="" />
+        </button>
+        <h4 className={animating ? 'copied-animating' : ''}>Copied!</h4>
+    </div>
+  )
+}
+
+export default CopyClipboardBtn
