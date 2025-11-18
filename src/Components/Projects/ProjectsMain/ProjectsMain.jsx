3598047400cece69c8db568109c9698ddadd4425
devangmalhotra <malh2226@mylaurier.ca>
2025-11-18 16:09:16 -0500

    updated files

--- src/Components/Projects/ProjectsMain/ProjectsMain.jsx
+++ src/Components/Projects/ProjectsMain/ProjectsMain.jsx
@@ -5,7 +5,7 @@ import ProjectCard from '../ProjectCard/ProjectCard'
 function ProjectsMain() {
   return (
     <div className='page'>
-        <ProjectCard name='A.I. Tic-Tac-Toe' description='Exploring the minimax, alpha-beta-pruning, and expectiminimax algorithms with a 3x3 board.' stack={["React", "Express", "PostgreSQL"]} />
+        <ProjectCard name='A.I. Tic-Tac-Toe' description='Exploring the minimax, alpha-beta-pruning, and expectiminimax algorithms with a 3x3 board.' stack={["React", "Express", "PostgreSQL"]} github-link='https://github.com/devangmalhotra/ai-tic-tac-toe' demo-link='https://devangmalhotra-tic-tac-toe.netlify.app/'/>
     </div>
   )
 }
