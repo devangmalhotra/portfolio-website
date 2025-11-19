82247ac39e4edc2afc07bcf4c2084a90f27b4c81
devangmalhotra <malh2226@mylaurier.ca>
2025-11-19 11:35:12 -0500

    updated files

--- src/Components/Projects/ProjectsNavigator/ProjectsNavigator.jsx
+++ src/Components/Projects/ProjectsNavigator/ProjectsNavigator.jsx
@@ -4,8 +4,12 @@ import './ProjectsNavigator.css'
 function ProjectsNavigator() {
   return (
     <div id='project-navigator-container' className=''>
-        <div id='project-navigator-back' className='card'></div>
-        <div id='project-navigator-forward' className='card'></div>
+        <div id='project-navigator-back' className='card'>
+            <div id='projects-navigator-backward-pic' className='icon'></div>
+        </div>
+        <div id='project-navigator-forward' className='card'>
+            <div id='projects-navigator-forward-pic' className='icon'></div>
+        </div>
     </div>
   )
 }
