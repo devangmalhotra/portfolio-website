3065c7b5025956c382d2090b2d5bd4611f655ae7
devangmalhotra <malh2226@mylaurier.ca>
2025-11-22 10:41:31 -0500

    updated files

--- src/Components/Experience/ExperiencesMain/ExperienceCard/ExperienceCard.jsx
+++ src/Components/Experience/ExperiencesMain/ExperienceCard/ExperienceCard.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './ExperienceCard.css'
 
-function ExperienceCard() {
+function ExperienceCard(props) {
   return (
-    <div className='experience-card card'>ExperienceCard</div>
+    <div className='experience-card card'>
+        <div className='experience-card-content'>
+            <h2>{props.title}</h2>
+            <div className='experience-description-container'>
+                <div className='experience-description-date-location'>
+                    <h4>{props.dateRange}</h4>
+                    <h4>{props.location}</h4>
+                </div>
+                <div className='experience-description-role-notes'>
+                    <ul>
+                        <li><p>• Test</p></li>
+                        <li><p>• Test</p></li>
+                        <li><p>• Test</p></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/Components/Experience/ExperiencesMain/ExperiencesMain.jsx
+++ src/Components/Experience/ExperiencesMain/ExperiencesMain.jsx
@@ -5,10 +5,10 @@ import ExperienceCard from './ExperienceCard/ExperienceCard'
 function ExperiencesMain() {
   return (
     <div id='experience-cards-container' className='page'>
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
+        <ExperienceCard title='Yamaha Motor Canada' dateRange='Sept 2025 — Dec 2025' location='Toronto, ON'/>
+        <ExperienceCard title='Yamaha Motor Canada' dateRange='Jan 2025 — Apr 2025' location='Toronto, ON'/>
+        <ExperienceCard title='Nissan Motor Corporation' dateRange='May 2024 — Aug 2024' location='Mississauga, ON'/>
+        <ExperienceCard title='Toronto Hydro' dateRange='May 2023 — Aug 2023' location='Toronto, ON'/>
     </div>
   )
 }
