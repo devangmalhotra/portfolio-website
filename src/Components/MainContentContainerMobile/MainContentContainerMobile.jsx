586afe43c8bdd9b58043fbf7d45507d1eff602f7
devangmalhotra <malh2226@mylaurier.ca>
2025-11-25 13:07:00 -0500

    updated files

--- src/Components/MainContentContainerMobile/MainContentContainerMobile.jsx
+++ src/Components/MainContentContainerMobile/MainContentContainerMobile.jsx
@@ -6,13 +6,14 @@ import GithubCard from '../Home/GithubCard/GithubCard'
 import LinkedinCard from '../Home/LinkedinCard/LinkedinCard'
 import LocationCard from '../Home/LocationCard/LocationCard'
 import NavBar from '../NavBar/NavBar'
+import { motion } from 'motion/react'
 
 function MainContentContainerMobile() {
   return (
     <div id='main-content-container-mobile'>
-        <div id='mobile-navbar-hamburger-container' className='card'>
+        <motion.div id='mobile-navbar-hamburger-container' className='card' initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.3, ease: [0, 0.71, 0.2, 1.01] }}>
             <div id='mobile-navbar-hamburger' className='icon'></div>
-        </div>
+        </motion.div>
         <div id='mobile-pages-container'>
             <HelloCard />
             <LocationCard />
