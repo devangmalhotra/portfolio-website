00e668d1584b079b47b2e379fea1065b075237d7
devangmalhotra <malh2226@mylaurier.ca>
2025-11-25 14:47:49 -0500

    updated files

--- src/Components/MainContentContainerMobile/MainContentContainerMobile.jsx
+++ src/Components/MainContentContainerMobile/MainContentContainerMobile.jsx
@@ -6,20 +6,56 @@ import GithubCard from '../Home/GithubCard/GithubCard'
 import LinkedinCard from '../Home/LinkedinCard/LinkedinCard'
 import LocationCard from '../Home/LocationCard/LocationCard'
 import NavBar from '../NavBar/NavBar'
+import ExperiencesMain from '../Experience/ExperiencesMain/ExperiencesMain'
+import ProjectsMain from '../Projects/ProjectsMain/ProjectsMain'
+import EmailCard from '../Contact/EmailCard/EmailCard'
+import PhoneCard from '../Contact/PhoneCard/PhoneCard'
+import DiscordCard from '../Contact/DiscordCard/DiscordCard'
 import { motion } from 'motion/react'
+import { useState } from 'react'
 
 function MainContentContainerMobile() {
+    const [activeSection, setActiveSection] = useState('home');
+
+    const sections = [{ name: 'home', content: (
+    <div id='home-page' className='page'>
+                <HelloCard />
+                <TechStackCard />
+                <LocationCard />
+                <GithubCard />
+                <LinkedinCard />
+            </div>
+            ) }, { name: 'experience', content: (
+    <div id='experiences-page'>
+                <ExperiencesMain />
+            </div>
+            ) }, { name: 'projects', content: (
+    <div id='projects-page'>
+                <ProjectsMain />
+            </div>
+            ) }, { name: 'contact', content: (
+    <div id='contact-page' className='page'>
+                <EmailCard />
+                <PhoneCard />
+                <DiscordCard />
+            </div>
+            ) }]
+
+    const renderActiveSection = () => {
+        return sections.find(section => section.name === activeSection).content
+    }
+
+    const handleHamburgerClick = () => {
+        
+    }
+
   return (
     <div id='main-content-container-mobile'>
-        <motion.div id='mobile-navbar-hamburger-container' className='card' initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.3, ease: [0, 0.71, 0.2, 1.01] }}>
+        <motion.div id='mobile-navbar-hamburger-container' className='card' initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.3, ease: [0, 0.71, 0.2, 1.01] }} onClick={handleHamburgerClick}>
             <div id='mobile-navbar-hamburger' className='icon'></div>
         </motion.div>
         <div id='mobile-pages-container'>
-            <HelloCard />
-            <LocationCard />
-            <TechStackCard />
-            <GithubCard />
-            <LinkedinCard />
+            {renderActiveSection()}
         </div>
     </div>
   )
