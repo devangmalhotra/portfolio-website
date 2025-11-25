1113db5ceaf8353de17b514c2745c0f439bd556f
devangmalhotra <malh2226@mylaurier.ca>
2025-11-25 10:52:10 -0500

    updated files

--- src/Components/MainContentContainerMobile/MainContentContainerMobile.jsx
+++ src/Components/MainContentContainerMobile/MainContentContainerMobile.jsx
@@ -5,10 +5,12 @@ import TechStackCard from '../Home/TechStackCard/TechStackCard'
 import GithubCard from '../Home/GithubCard/GithubCard'
 import LinkedinCard from '../Home/LinkedinCard/LinkedinCard'
 import LocationCard from '../Home/LocationCard/LocationCard'
+import NavBar from '../NavBar/NavBar'
 
 function MainContentContainerMobile() {
   return (
     <div id='main-content-container-mobile'>
+        <div id='mobile-navbar-hamburger'></div>
         <HelloCard />
         <LocationCard />
         <TechStackCard />
