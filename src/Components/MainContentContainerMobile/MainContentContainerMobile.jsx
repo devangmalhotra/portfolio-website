3e7017533866fded9d085e945886d1ffda62cdbf
devangmalhotra <malh2226@mylaurier.ca>
2025-11-25 10:49:11 -0500

    updated files

--- src/Components/MainContentContainerMobile/MainContentContainerMobile.jsx
+++ src/Components/MainContentContainerMobile/MainContentContainerMobile.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import './MainContentContainerMobile.css'
 import HelloCard from '../Home/HelloCard/HelloCard'
 import TechStackCard from '../Home/TechStackCard/TechStackCard'
+import GithubCard from '../Home/GithubCard/GithubCard'
+import LinkedinCard from '../Home/LinkedinCard/LinkedinCard'
+import LocationCard from '../Home/LocationCard/LocationCard'
 
 function MainContentContainerMobile() {
   return (
     <div id='main-content-container-mobile'>
         <HelloCard />
+        <LocationCard />
         <TechStackCard />
+        <GithubCard />
+        <LinkedinCard />
     </div>
   )
 }
