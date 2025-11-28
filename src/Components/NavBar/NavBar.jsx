5fd62fc985201804990c3f356d31fe7701f187bf
devangmalhotra <malh2226@mylaurier.ca>
2025-11-27 23:01:09 -0500

    updated files

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -8,10 +8,10 @@ function NavBar(props) {
     <motion.div id='navbar-card-container' className='card' initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.2 }} >
         <nav className='navbar card-content'>
           <ul className='navbar-links'>
-              <li onClick={props.handleSectionClick.bind(this, 'home')} className={props.activeSection == 'home' ? 'navbar-active' : ''}><div className='navbar-icon icon' id='home-icon'></div>Home</li>
-              <li onClick={props.handleSectionClick.bind(this, 'experience')} className={props.activeSection == 'experience' ? 'navbar-active' : ''}><div className='navbar-icon icon' id='experience-icon'></div>Experience</li>
-              <li onClick={props.handleSectionClick.bind(this, 'projects')} className={props.activeSection == 'projects' ? 'navbar-active' : ''}><div className='navbar-icon icon' id='projects-icon'></div>Projects</li>
-              <li onClick={props.handleSectionClick.bind(this, 'contact')} className={props.activeSection == 'contact' ? 'navbar-active' : ''}><div className='navbar-icon icon' id='contact-icon'></div>Contact</li>
+              <li onClick={() => props.handleSectionClick('home')}className={props.activeSection == 'home' ? 'navbar-active' : ''}><div className='navbar-icon icon' id='home-icon'></div>Home</li>
+              <li onClick={() => {props.handleSectionClick('experience')}} className={props.activeSection == 'experience' ? 'navbar-active' : ''}><div className='navbar-icon icon' id='experience-icon'></div>Experience</li>
+              <li onClick={() => props.handleSectionClick('projects')} className={props.activeSection == 'projects' ? 'navbar-active' : ''}><div className='navbar-icon icon' id='projects-icon'></div>Projects</li>
+              <li onClick={() => props.handleSectionClick('contact')} className={props.activeSection == 'contact' ? 'navbar-active' : ''}><div className='navbar-icon icon' id='contact-icon'></div>Contact</li>
           </ul>
         </nav>
     </motion.div>
