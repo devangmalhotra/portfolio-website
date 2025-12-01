24ee0b093fb91c40a6e0da73d0bfe111ffec36c4
devangmalhotra <malh2226@mylaurier.ca>
2025-11-30 23:02:45 -0500

    updated files

--- src/Components/LightDarkToggle/LightDarkToggle.jsx
+++ src/Components/LightDarkToggle/LightDarkToggle.jsx
@@ -10,7 +10,7 @@ function LightDarkToggle() {
 
   return (
     <div id='light-dark-toggle-container'>
-        <button id='light-dark-button' onClick={handleLightDarkClick}>{`${mode} Mode`}</button>
+        <button id='light-dark-button' onClick={handleLightDarkClick}><div id='mode-icon' className={`${mode.toLowerCase()}-mode icon`}></div>{`${mode} Mode`}</button>
     </div>
   )
 }
