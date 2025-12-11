b3e96193ebe9f806b4f5a519af0dee9bc62b9598
devangmalhotra <malh2226@mylaurier.ca>
2025-12-10 19:33:30 -0500

    updated files

--- src/Components/Home/HelloCard/HelloCard.jsx
+++ src/Components/Home/HelloCard/HelloCard.jsx
@@ -8,7 +8,7 @@ function HelloCard() {
     <motion.div id='hello-card-container' className='card' initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.3, ease: [0, 0.71, 0.2, 1.01] }}>
       <div id='hello-card-content' className='card-content'>
         <h2>Hi There, I'm Devang Malhotra! 👋</h2>
-        <h5>4th Year BBA and CS Student @ Wilfid Laurier University</h5>
+        <h5>4th Year BBA and CS Student @ Wilfrid Laurier University</h5>
         <ContactBtn />
       </div>
     </motion.div>
